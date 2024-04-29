05aa333d31fedfc8e1c7204f45b10c44024cec38
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2024-04-29 13:24:06 +0200

    Update ovh-api-get.py

--- bin/ovh-api-get.py
+++ bin/ovh-api-get.py
@@ -2,7 +2,7 @@
 
 # -*- encoding: utf-8 -*-
 
-# Usage: /usr/local/bin/futur-tech-zabbix-ovh/ovh-api-get.py <conf_name (without .conf)> <api_path> <loop_api_path>
+# Usage: /usr/lib/zabbix/externalscripts/ovh-api-get.py <conf_name (without .conf)> <api_path> <loop_api_path>
 # <loop_api_path> Should be used when the <api_path> return a list which should be looped through replacing #loop#
 # Example:
 # /usr/lib/zabbix/externalscripts/ovh-api-get.py default_api /email/domain ## This will list email domains
@@ -32,7 +32,7 @@ config.read(conf_path)
 # You can generate new credentials with full access to your account on
 # the token creation page https://api.ovh.com/createToken/index.cgi?GET=/*&PUT=/*&POST=/*&DELETE=/*
 client = ovh.Client(
-    endpoint=config['OVH_API']['endpoint'],               # Endpoint of API OVH Europe (List of available endpoints https://github.com/ovh/python-ovh#2-configure-your-application)
+    endpoint=config['OVH_API']['endpoint'],               # Endpoint of API OVH (List of available endpoints https://github.com/ovh/python-ovh#2-configure-your-application)
     application_key=config['OVH_API']['application_key'],    # Application Key
     application_secret=config['OVH_API']['application_secret'], # Application Secret
     consumer_key=config['OVH_API']['consumer_key'],       # Consumer Key
